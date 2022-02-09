10756693802b85676bd9e0caeb70c3adff285518
ahmadnura-dev <99246265+ahmadnura-dev@users.noreply.github.com>
2022-02-09 09:17:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if(x.style.dispaly === "block"){
+        x.style.display = "none";
+    }else {
+        x.style.display = "block";
+    }
+}
